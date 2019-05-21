426a18258f8c6b6959d3db4cf7caadaddd5af566
Barry Fulk <bfulk@Barrys-MacBook-Pro.local>
2019-05-21 16:23:52 -0600

    attribute binding

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 var App = new Vue({
-  el: '#app',
+  el: "#app",
   data: {
-    product: 'Socks',
-    description: 'These socks are made from merino wool interwoven with Nylon for more durability.'
+    product: "Socks",
+    image: "./assets/vmSocks-green-onWhite.jpg",
+    link:
+      "https://images.unsplash.com/photo-1553460588-3ba256b9aac9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
   }
 });
-
