b2c015c9a137982502d7d0d9b6d826304fc06f5c
Barry Fulk <bfulk@Barrys-MacBook-Pro.local>
2019-05-21 16:39:52 -0600

    conditional render

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ var App = new Vue({
   data: {
     product: "Socks",
     image: "./assets/vmSocks-green-onWhite.jpg",
-    link:
-      "https://images.unsplash.com/photo-1553460588-3ba256b9aac9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+    inventory: 0
   }
 });
