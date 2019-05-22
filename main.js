39ba5d4442e6976e8f8b8cc4bd274980f50c106d
Barry Fulk <bfulk@Barrys-MacBook-Pro.local>
2019-05-22 10:01:43 -0600

    list rendering

--- main.js
+++ main.js
@@ -3,6 +3,17 @@ var App = new Vue({
   data: {
     product: "Socks",
     image: "./assets/vmSocks-green-onWhite.jpg",
-    inventory: 0
+    inStock: true,
+    details: ["80% cotton", "20% polyester", 'Gender-neutral'],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green'
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue'
+      }
+    ]
   }
 });
